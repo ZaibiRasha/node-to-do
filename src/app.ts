d74cbb78eb010d81f636025ebbca845730ad1825
ZaibiRasha <racha.zaibi@wind-consulting-tunisia.com>
2023-04-14 11:23:59 +0100

    chnge connections function

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { router } from './routes';
 import authApp from './middlewares/authenticate'; // Import authApp instead of app
+import sequelize from './sequelize';
 
 const app = express();
 const port = process.env.PORT;
@@ -11,7 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(router);
 app.use(authApp);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
+const connexion = (async () => {
+  await sequelize
+          .sync({ force: false })
+          .then(() => console.log('Database and tables created successfully'))
+          .catch((err) => console.log('Error while creating database', err));
+  app.listen(port, () => console.info(`Server running on port ${port}`));
+})();
+export { connexion };
