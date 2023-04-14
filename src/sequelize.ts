2c41a83760668828320ed59f976bec38a67da13a
ZaibiRasha <racha.zaibi@wind-consulting-tunisia.com>
2023-04-14 12:37:07 +0100

    delete model Todo

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -2,13 +2,12 @@ import { Sequelize } from 'sequelize-typescript';
 import config from './config/config';
 import { User } from './models/User';
 import { Task } from './models/Task';
-import { Todo } from './models/Todo';
 
 const { dialect, database, username, password } = config.db;
 
 export const sequelize = new Sequelize(database, username, password, {
   dialect,
-  models: [User, Task, Todo],
+  models: [User, Task],
 });
 
 export default sequelize;
