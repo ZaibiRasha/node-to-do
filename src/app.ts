353449123141b46b9c05f9d434acedfde0137e50
ZaibiRasha <racha.zaibi@wind-consulting-tunisia.com>
2023-04-16 00:59:34 +0100

    optimisation & wording

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { router } from './routes';
-import authApp from './middlewares/authenticate'; // Import authApp instead of app
+import  router  from './routes';
+// import authApp from './middlewares/authenticate'; // Import authApp instead of app
 import sequelize from './sequelize';
 
 const app = express();
@@ -10,7 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
-app.use(authApp);
+// app.use(authApp);
+
 
 const connexion = (async () => {
   await sequelize
--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,75 +1,35 @@
-import express, { NextFunction } from 'express';
-import session, { Session, SessionData } from 'express-session';
-import { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/User';
 
-dotenv.config();
-const { SESSION_SECRET } = process.env;
 
-interface MySession extends SessionData {
+interface AuthRequest extends Request {
   user?: any;
 }
 
-const authApp  = express();
-
-// Configure express-session middleware
-authApp .use(
-  session({
-    secret: SESSION_SECRET || '',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false },
-  })
-);
-function authenticate(req: Request, res: Response, next: NextFunction) {
-  const user = (req.session as MySession).user;
-  if (!user) {
-    return res.status(401).json({ message: 'You must be logged in to access this resource.' });
-  }
-  next();
-}
-
-// Your route handlers and other middleware
-authApp .get('/', authenticate, (req: Request, res: Response) => {
-  // This route is protected by the authenticate middleware
-  // Only authenticated users can access this route
-  const user = (req.session as MySession).user;
-  res.send(`Hello, authenticated user ${user.username}!`);
-});
-
-// authApp .get('/login', (req: Request, res: Response) => {
-//   // Render the login page
-//   res.render('login');
-// });
-
-authApp.post('/login', authenticate, (req: Request, res: Response, next: NextFunction) => {
-  // Authenticate the user and set the session data
-  const user = authenticate(req.body.username, req.body.password, next);
-
-  if (user) {
-    (req.session as MySession).user = user;
-    res.redirect('/');
-  } else {
-    res.render('login', { error: 'Invalid username or password' });
-  }
-});
-
-
-authApp.get('/logout', (req: Request, res: Response) => {
-  // Clear the session data and redirect to the login page
-  req.session.destroy((err) => {
-    if (err) {
-      console.error(err);
+export const authMiddleware = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.rawHeaders[5];
+    const token = authHeader?.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized 1' });
     }
-    res.redirect('/login');
-  });
-});
+    const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET!);
+    const userId : number = decodedToken.userId;
+    console.log(userId);
 
-authApp .get('/tasks',authenticate, (req: Request, res: Response) => {
-  // This route is protected by the authenticate middleware
-  // Only authenticated users can access this route
-  const user = (req.session as MySession).user;
-  res.send('/tasks');
-});
-
-export default authApp ;
+    const user = await User.findOne({ where: { id: userId } });
+    if (!user) {
+      return res.status(401).json({ message: 'Unauthorized 2' });
+    }
+    req.user = user;
+    next();
+  } catch (error : any) {
+    console.error(error.message);
+    return res.status(401).json({ message:  'Unauthorized 3' });
+  }
+};
